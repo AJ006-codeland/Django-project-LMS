6722c81017dc786e5789d05a34ea3c7d6d52212f
AJ006-codeland <aaj.mnr@gmail.com>
2025-06-18 14:51:11 +0545

    Django Project day 9: teacher listing, bootstrap css use

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
-from core.views import HomePageView,TeacherCreateView,TeacherListView
+from core.views import HomePageView,TeacherCreateView,TeacherListView, TeacherDeleteView, TeacherDetailView, TeacherUpdateView
 urlpatterns =[
     path('',HomePageView.as_view, name='home'),
     path('teacher/create/', TeacherCreateView.as_view(), name= 'teacher.create'),
-    path('teacher/list/', TeacherListView.as_view(), name='teacher.index')
+    path('teacher/list/', TeacherListView.as_view(), name='teacher.index'),
+    path('teacher/<int:pk>/detail/', TeacherDetailView.as_view(), name= 'teacher.detail'),
+    path('teacher/<int:pk>/delete/', TeacherDeleteView.as_view(), name= 'teacher.delete'),
+    path('teacher/<int:pk>/update/', TeacherUpdateView.as_view(), name= 'teacher.update'),
 ]
