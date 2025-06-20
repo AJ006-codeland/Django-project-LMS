0d852cfcea6ba8ede43b56b55327c36fe7ca2586
AJ006-codeland <aaj.mnr@gmail.com>
2025-06-20 15:27:32 +0545

    Django Project day 11: Assignment CRUD + signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from accounts.views import ProfileView, SignupView
 
-from accounts.views import ProfileView
-
-urlpatterns=[
-    path('profile/', ProfileView.as_view(), name='profile')
+urlpatterns = [
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('signup/', SignupView.as_view(), name ='signup')
 ]
